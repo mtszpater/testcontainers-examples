bf9512cd3187bf67eca44055c5a84aeaabf7911c
Mateusz Pater <mtszpater@gmail.com>
2020-03-14 16:37:42 +0100

    initial commit

---
+++ src/test/java/dev/pater/testcontainersexample/TestcontainersExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pater.testcontainersexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
